d4f4b14701a3e10faad80e053f093f5342ca55bb
Byte-A <66559821+Byte-A@users.noreply.github.com>
2020-11-11 18:03:07 -0500

    Add files via upload

---
+++ projects/dndice/diceroller.js
@@ -0,0 +1,35 @@
+         function rolld20() {
+            var a = Math.floor((Math.random() * 20));
+           d20=a.toString();
+           document.getElementById("rolld20").value = d20;
+         }
+         function rolld12() {
+            var b = Math.floor((Math.random() * 12));
+           d12=b.toString();
+           document.getElementById("rolld12").value = d12;
+         }
+         function rolld10() {
+            var c = Math.floor((Math.random() * 10));
+           d10=c.toString();
+           document.getElementById("rolld10").value = d10;
+         }
+         function rolld100() {
+            var d = Math.floor((Math.random() * 100));
+           d100=d.toString();
+           document.getElementById("rolld100").value = d100;
+         }
+         function rolld8() {
+           var e = Math.floor((Math.random() * 8));
+           d8=e.toString();
+           document.getElementById("rolld8").value = d8;
+         }
+         function rolld6() {
+           var f = Math.floor((Math.random() * 6));
+           d6=f.toString();
+           document.getElementById("rolld6").value = d6;
+         }
+         function rolld4() {
+           var g = Math.floor((Math.random() * 4));
+           d4=g.toString();
+           document.getElementById("rolld4").value = d4;
+         }
